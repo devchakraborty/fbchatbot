5fd11601c7441832351243a6b7636e83076afee9
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2017-01-11 21:28:53 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,106 @@
+import EventEmitter from 'events';
+import express from 'express';
+import bodyParser from 'body-parser';
+import winston from 'winston';
+import _ from 'lodash';
+import Promise from 'bluebird';
+import fetch from 'node-fetch';
+
+export class FBBot extends EventEmitter {
+    constructor(options={}) {
+        super();
+        this.options = options;
+        this.app = express();
+        this.app.use(bodyParser.json());
+
+        const verifyToken = options.verifyToken || process.env.FBBOT_VERIFY_TOKEN || 'fbbot';
+
+        this.app.get('/webhook', (req, res) => {
+            if (req.query['hub.mode'] == 'subscribe') {
+                if (req.query['hub.verify_token'] == verifyToken) {
+                    this.logger.info('Got valid subscription request');
+                    res.status(200).send(req.query['hub.challenge']);
+                } else {
+                    this.logger.warn('Got invalid subscription request');
+                    res.sendStatus(403);
+                }
+            } else {
+                this.logger.warn('Got unsupported webhook GET request');
+                res.sendStatus(404);
+            }
+        });
+
+        const EVENT_TYPES = [
+            'pre_checkout',
+            'message',
+            'delivery',
+            'read',
+            'postback',
+            'optin',
+            'referral',
+            'payment',
+            'checkout_update',
+            'account_linking'
+        ];
+
+        this.app.post('/webhook', (req, res) => {
+            res.sendStatus(200);
+
+            this.logger.debug('Webhook POST from FB', JSON.stringify(req.body, null, '  '));
+
+            if (req.body.object != 'page') {
+                this.logger.warn('Got non-page webhook POST request');
+                return;
+            }
+
+            const entries = req.body.entry;
+            if (!entries) {
+                return;
+            }
+
+            entries.forEach((entry) => {
+                const messaging = entry.messaging;
+                if (!messaging) {
+                    return;
+                }
+
+
+                messaging.forEach((messagingEvent) => {
+                    for (const EVENT_TYPE of EVENT_TYPES) {
+                        if (messagingEvent[EVENT_TYPE]) this.emit(EVENT_TYPE, messagingEvent);
+                    }
+                });
+            });
+        });
+
+        this.logger = new (winston.Logger)({
+            transports: [
+              new (winston.transports.Console)({
+                'timestamp':true,
+                level: options.logLevel || 'info'
+              })
+            ]
+        });
+    }
+
+    listen() {
+        this.app.listen(this.options.port || process.env.PORT || 3000);
+    }
+
+    sendMessage(message) {
+        const accessToken = this.options.accessToken || process.env.FBBOT_ACCESS_TOKEN;
+
+        if (!accessToken) return Promise.reject('No page access token provided');
+
+        return fetch(
+            `https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                },
+                body: JSON.stringify(message)
+            }
+        );
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,100 @@
+import request from 'supertest';
+import sinon from 'sinon';
+import nock from 'nock';
+import { FBBot } from '../lib';
+
+function botFactory() {
+    return new FBBot({
+        verifyToken: 'verify_token',
+        logLevel: 'error',
+        accessToken: 'access_token'
+    });
+}
+
+describe('FBBot', () => {
+
+    it('responds to correct subscription', (done) => {
+        const bot = botFactory();
+        request(bot.app)
+            .get('/webhook?hub.mode=subscribe&hub.verify_token=verify_token&hub.challenge=abcdef')
+            .expect(200, 'abcdef', done);
+    });
+
+    it('responds to incorrect subscription', (done) => {
+        const bot = botFactory();
+        request(bot.app)
+            .get('/webhook?hub.mode=subscribe&hub.verify_token=verfy_token&hub.challenge=abcdef')
+            .expect(403, done);
+    });
+
+    it('responds to valid message event', (done) => {
+        const bot = botFactory();
+        const spy = sinon.spy();
+
+        bot.on('message', spy);
+
+        const message = {
+            sender: {
+                id: 'USER_ID'
+            },
+            recipient: {
+                id: 'PAGE_ID'
+            },
+            timestamp: 1458692752478,
+            message: {
+                mid: 'mid.1457764197618:41d102a3e1ae206a38',
+                seq: 73,
+                text: 'hello, world!',
+                quick_reply: {
+                    payload: 'DEVELOPER_DEFINED_PAYLOAD'
+                }
+            }
+        };
+
+        request(bot.app)
+            .post('/webhook')
+            .send({
+                object: 'page',
+                entry: [
+                    {
+                        id: 'PAGE_ID',
+                        time: 1458692752478,
+                        messaging: [
+                            message
+                        ]
+                    }
+                ]
+            })
+            .expect(200)
+            .then(() => {
+                sinon.assert.calledOnce(spy);
+                sinon.assert.calledWith(spy, message);
+                done();
+            });
+    });
+
+    it('sends message correctly', (done) => {
+        const bot = botFactory();
+
+        const fb = nock('https://graph.facebook.com')
+                    .post('/v2.6/me/messages')
+                    .query({ access_token: 'access_token' })
+                    .reply(200, {
+                        recipient_id: '1008372609250235',
+                        message_id: 'mid.1456970487936:c34767dfe57ee6e339'
+                    });
+
+        const message = {
+            recipient: {
+                id: 'USER_ID'
+            },
+            message: {
+                text: 'hello, world!'
+            }
+        };
+
+        bot.sendMessage(message).then(() => {
+            fb.done();
+        }).then(done);
+    });
+});
